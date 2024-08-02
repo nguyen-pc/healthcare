2939b7cafd6e2cfb523d64bc15b5064a3fd6cc28
nguyen <nguyenthcs430@gmail.com>
2024-08-02 20:27:43 +0700

    data table 2

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,7 +11,7 @@ import {columns, Payment} from "@/components/table/columns";
 const Admin = async () => {
 
   const appointments = await getRecentAppointmentList();
-
+  // console.log(appointments)
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
